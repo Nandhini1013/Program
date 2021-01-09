15a4d5ba7a627a1e08544a62c1d17101a6d59080
nandhini1013 <nandhinismiley98@gmail.com>
2021-01-09 20:22:46 +0530

    added

--- src/test/java/org/test/NullProgram.java
+++ src/test/java/org/test/NullProgram.java
@@ -2,10 +2,15 @@ package org.test;
 
 public class NullProgram {
 
-	String s=null;
+	String s="value";
 	
 	public void Emp() {
     System.out.println(s);
+	}
+	
+	private void Comp() {
+System.out.println("company");
+
 	}
 	
 	public static void main(String[] args) {
